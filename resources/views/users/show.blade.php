40813490476443937e3dd51e647c565e4ae0703d
eduardonoguerag <eduardo.nogueraga@gmail.com>
2021-05-08 16:32:41 +0000

    Ampliados detalles del usuario

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -6,7 +6,13 @@
 <h1>Usuario #{{ $user->id }}</h1>
 
 <p>Nombre del usuario: {{ $user->name }}</p>
+<p>Apellidos del usuario: {{$user->last}}</p>
 <p>Correo electrónico: {{ $user->email }}</p>
+<p>Twitter: {{ ($user->profile->twitter)?: 'Sin cuenta de twitter' }}</p>
+<p>Profession: {{($user->profile->profession->title)?: 'Sin profesion'}}</p>
+<p>Equipo: {{($user->team->name)?: 'Sin equipo'}}</p>
+<p>Hablidades: {{($user->skills->implode('name', ','))?: 'Sin habilidades'}}</p>
+
 <p>
     <a href="{{ route('users.index') }}">Regresar al listado de usuarios</a>
 </p>
