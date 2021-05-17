1369a9d84812569d271546639122178d93068a05
eduardonoguerag <eduardo.nogueraga@gmail.com>
2021-05-17 19:17:38 +0000

    Pruebas para formularios de profesiones create y update

--- tests/Feature/Admin/CreateProfessionTest.php
+++ tests/Feature/Admin/CreateProfessionTest.php
@@ -61,4 +61,160 @@ class CreateProfessionTest extends TestCase
 
         $this->assertDatabaseEmpty('professions');
     }
+
+    /** @test */
+    function the_salary_field_is_required()
+    {
+        $this->handleValidationExceptions();
+        $this->from('profesiones/create')
+            ->post('profesiones/', $this->withData([
+                'salary' => null
+            ]))->assertSessionHasErrors(['salary'])
+            ->assertRedirect('profesiones/create');
+
+        $this->assertDatabaseEmpty('professions');
+    }
+
+    /** @test */
+    function the_salary_field_must_be_numeric()
+    {
+        $this->handleValidationExceptions();
+
+        $this->from('profesiones/create')
+            ->post('profesiones/', $this->withData([
+                'salary' => 'Volvamos al trueque'
+            ]))->assertSessionHasErrors(['salary'])
+            ->assertRedirect('profesiones/create');
+
+        $this->assertDatabaseEmpty('professions');
+    }
+
+    /** @test */
+    function the_workday_field_is_required()
+    {
+        $this->handleValidationExceptions();
+        $this->from('profesiones/create')
+            ->post('profesiones/', $this->withData([
+                'workday' => null
+            ]))->assertSessionHasErrors(['workday'])
+            ->assertRedirect('profesiones/create');
+
+        $this->assertDatabaseEmpty('professions');
+    }
+
+    /** @test */
+    function the_workday_field_must_be_valid()
+    {
+        $this->handleValidationExceptions();
+        $this->from('profesiones/create')
+            ->post('profesiones/', $this->withData([
+                'workday' => 'Jornada laboral de media hora'
+            ]))->assertSessionHasErrors(['workday'])
+            ->assertRedirect('profesiones/create');
+        $this->assertDatabaseEmpty('professions');
+    }
+
+    /** @test */
+    function the_language_field_is_required()
+    {
+        $this->handleValidationExceptions();
+        $this->from('profesiones/create')
+            ->post('profesiones/', $this->withData([
+                'language' => null
+            ]))->assertSessionHasErrors(['language'])
+            ->assertRedirect('profesiones/create');
+        $this->assertDatabaseEmpty('professions');
+    }
+
+    /** @test */
+    function the_language_field_must_be_valid()
+    {
+        $this->handleValidationExceptions();
+        $this->from('profesiones/create')
+            ->post('profesiones/', $this->withData([
+                'language' => '999'
+            ]))->assertSessionHasErrors(['language'])
+            ->assertRedirect('profesiones/create');
+        $this->assertDatabaseEmpty('professions');
+    }
+
+
+    /** @test */
+    function the_vehicle_field_is_required()
+    {
+        $this->handleValidationExceptions();
+        $this->from('profesiones/create')
+            ->post('profesiones/', $this->withData([
+                'vehicle' => null
+            ]))->assertSessionHasErrors(['vehicle'])
+            ->assertRedirect('profesiones/create');
+        $this->assertDatabaseEmpty('professions');
+    }
+
+    /** @test */
+    function the_vehicle_field_must_be_valid()
+    {
+        $this->handleValidationExceptions();
+        $this->from('profesiones/create')
+            ->post('profesiones/', $this->withData([
+                'vehicle' => '999'
+            ]))->assertSessionHasErrors(['vehicle'])
+            ->assertRedirect('profesiones/create');
+        $this->assertDatabaseEmpty('professions');
+    }
+
+    /** @test */
+    function the_academic_level_field_is_required()
+    {
+        $this->handleValidationExceptions();
+        $this->from('profesiones/create')
+            ->post('profesiones/', $this->withData([
+                'academic_level' => null
+            ]))->assertSessionHasErrors(['academic_level'])
+            ->assertRedirect('profesiones/create');
+
+        $this->assertDatabaseEmpty('professions');
+    }
+
+    /** @test */
+    function the_academic_level_field_must_be_valid()
+    {
+        $this->handleValidationExceptions();
+        $this->from('profesiones/create')
+            ->post('profesiones/', $this->withData([
+                'academic_level' => 'Universidad de la vida'
+            ]))->assertSessionHasErrors(['academic_level'])
+            ->assertRedirect('profesiones/create');
+        $this->assertDatabaseEmpty('professions');
+    }
+
+    /** @test */
+    function the_experience_field_is_optional()
+    {
+        $this->from('profesiones/create')
+            ->post('profesiones/', $this->withData([
+                'experience' => null
+            ]))->assertRedirect('profesiones');
+        $this->assertDatabaseCount('professions', 1);
+
+        $this->assertDatabaseHas('professions', [
+            'title' => 'Desarrollador web',
+            'salary' => '15000',
+            'workday' => 'Jornada completa',
+        ]);
+    }
+
+    /** @test */
+    function the_experience_field_must_be_valid()
+    {
+        $this->handleValidationExceptions();
+        $this->from('profesiones/create')
+            ->post('profesiones/', $this->withData([
+                'experience' => '0'
+            ]))->assertSessionHasErrors(['experience'])
+            ->assertRedirect('profesiones/create');
+
+        $this->assertDatabaseEmpty('professions');
+    }
+
 }
--- tests/Feature/Admin/UpdateProfessionTest.php
+++ tests/Feature/Admin/UpdateProfessionTest.php
@@ -74,4 +74,229 @@ class UpdateProfessionTest extends TestCase
         ]);
     }
 
+    /** @test  */
+    function the_salary_field_is_required()
+    {
+        $profession = Profession::factory()->create();
+
+        $this->handleValidationExceptions();
+
+        $this->from("profesiones/{$profession->id}/editar")
+            ->put("profesiones/{$profession->id}", $this->withData([
+                'salary' => null,
+            ]))->assertSessionHasErrors(['salary'])
+            ->assertRedirect("profesiones/{$profession->id}/editar");
+
+        $this->assertDatabaseHas('professions', [
+            'id' => $profession->id,
+            'title' => $profession->title,
+            'salary' => $profession->salary,
+        ]);
+    }
+
+    /** @test */
+    function the_salary_field_must_be_numeric()
+    {
+        $profession = Profession::factory()->create();
+
+        $this->handleValidationExceptions();
+
+        $this->from("profesiones/{$profession->id}/editar")
+            ->put("profesiones/{$profession->id}", $this->withData([
+                'salary' => 'Volvamos al trueque',
+            ]))->assertSessionHasErrors(['salary'])
+            ->assertRedirect("profesiones/{$profession->id}/editar");
+
+        $this->assertDatabaseHas('professions', [
+            'id' => $profession->id,
+            'title' => $profession->title,
+            'salary' => $profession->salary,
+        ]);
+    }
+
+    /** @test */
+    function the_workday_field_is_required()
+    {
+        $profession = Profession::factory()->create();
+        $this->handleValidationExceptions();
+
+        $this->from("profesiones/{$profession->id}/editar")
+            ->put("profesiones/{$profession->id}", $this->withData([
+                'workday' => null,
+            ]))->assertSessionHasErrors(['workday'])
+            ->assertRedirect("profesiones/{$profession->id}/editar");
+
+        $this->assertDatabaseHas('professions', [
+            'id' => $profession->id,
+            'title' => $profession->title,
+            'workday' => $profession->workday,
+        ]);
+    }
+
+    /** @test */
+    function the_workday_field_must_be_valid()
+    {
+        $profession = Profession::factory()->create();
+        $this->handleValidationExceptions();
+
+        $this->from("profesiones/{$profession->id}/editar")
+            ->put("profesiones/{$profession->id}", $this->withData([
+                'workday' => 'Jornada laboral de media hora'
+            ]))->assertSessionHasErrors(['workday'])
+            ->assertRedirect("profesiones/{$profession->id}/editar");
+
+        $this->assertDatabaseHas('professions', [
+            'id' => $profession->id,
+            'title' => $profession->title,
+            'workday' => $profession->workday,
+        ]);
+    }
+
+    /** @test */
+    function the_language_field_is_required()
+    {
+        $profession = Profession::factory()->create();
+        $this->handleValidationExceptions();
+
+        $this->from("profesiones/{$profession->id}/editar")
+            ->put("profesiones/{$profession->id}", $this->withData([
+                'language' => null,
+            ]))->assertSessionHasErrors(['language'])
+            ->assertRedirect("profesiones/{$profession->id}/editar");
+
+        $this->assertDatabaseHas('professions', [
+            'id' => $profession->id,
+            'title' => $profession->title,
+            'language' => $profession->language
+        ]);
+    }
+
+    /** @test */
+    function the_language_field_must_be_valid()
+    {
+        $profession = Profession::factory()->create();
+        $this->handleValidationExceptions();
+
+        $this->from("profesiones/{$profession->id}/editar")
+            ->put("profesiones/{$profession->id}", $this->withData([
+                'language' => '999',
+            ]))->assertSessionHasErrors(['language'])
+            ->assertRedirect("profesiones/{$profession->id}/editar");
+
+        $this->assertDatabaseHas('professions', [
+            'id' => $profession->id,
+            'title' => $profession->title,
+            'language' => $profession->language
+        ]);
+    }
+
+    /** @test */
+    function the_vehicle_field_is_required()
+    {
+        $profession = Profession::factory()->create();
+        $this->handleValidationExceptions();
+
+        $this->from("profesiones/{$profession->id}/editar")
+            ->put("profesiones/{$profession->id}", $this->withData([
+                'vehicle' => null,
+            ]))->assertSessionHasErrors(['vehicle'])
+            ->assertRedirect("profesiones/{$profession->id}/editar");
+
+        $this->assertDatabaseHas('professions', [
+            'id' => $profession->id,
+            'title' => $profession->title,
+            'vehicle' => $profession->vehicle
+        ]);
+    }
+
+    /** @test */
+    function the_vehicle_field_must_be_valid()
+    {
+        $profession = Profession::factory()->create();
+        $this->handleValidationExceptions();
+
+        $this->from("profesiones/{$profession->id}/editar")
+            ->put("profesiones/{$profession->id}", $this->withData([
+                'vehicle' => '999',
+            ]))->assertSessionHasErrors(['vehicle'])
+            ->assertRedirect("profesiones/{$profession->id}/editar");
+
+        $this->assertDatabaseHas('professions', [
+            'id' => $profession->id,
+            'title' => $profession->title,
+            'vehicle' => $profession->vehicle
+        ]);
+    }
+
+    /** @test */
+    function the_academic_level_field_is_required()
+    {
+        $profession = Profession::factory()->create();
+        $this->handleValidationExceptions();
+
+        $this->from("profesiones/{$profession->id}/editar")
+            ->put("profesiones/{$profession->id}", $this->withData([
+                'academic_level' => null,
+            ]))->assertSessionHasErrors(['academic_level'])
+            ->assertRedirect("profesiones/{$profession->id}/editar");
+
+        $this->assertDatabaseHas('professions', [
+            'id' => $profession->id,
+            'title' => $profession->title,
+            'academic_level' => $profession->academic_level,
+        ]);
+    }
+
+    /** @test */
+    function the_academic_level_field_must_be_valid()
+    {
+        $profession = Profession::factory()->create();
+        $this->handleValidationExceptions();
+
+        $this->from("profesiones/{$profession->id}/editar")
+            ->put("profesiones/{$profession->id}", $this->withData([
+                'academic_level' => 'La universidad de la vida',
+            ]))->assertSessionHasErrors(['academic_level'])
+            ->assertRedirect("profesiones/{$profession->id}/editar");
+
+        $this->assertDatabaseHas('professions', [
+            'id' => $profession->id,
+            'title' => $profession->title,
+            'academic_level' => $profession->academic_level,
+        ]);
+    }
+
+    /** @test */
+    function the_experience_field_is_optional()
+    {
+        $profession = Profession::factory()->create();
+
+        $this->from("profesiones/{$profession->id}/editar")
+            ->put("profesiones/{$profession->id}", $this->withData([
+                'experience' => null,
+            ]))->assertRedirect("profesiones/{$profession->id}/show");
+
+        $this->assertDatabaseHas('professions', [
+            'id' => $profession->id,
+            'experience' => null,
+        ]);
+    }
+
+    /** @test */
+    function the_experience_field_must_be_valid()
+    {
+        $profession = Profession::factory()->create();
+        $this->handleValidationExceptions();
+
+        $this->from("profesiones/{$profession->id}/editar")
+            ->put("profesiones/{$profession->id}", $this->withData([
+                'experience' => '0',
+            ]))->assertSessionHasErrors(['experience'])
+            ->assertRedirect("profesiones/{$profession->id}/editar");
+
+        $this->assertDatabaseHas('professions', [
+            'id' => $profession->id,
+            'experience' => $profession->experience
+        ]);
+    }
 }
