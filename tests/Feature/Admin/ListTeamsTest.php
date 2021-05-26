f17eebce035bb7d29ad8ac4dd405da30363504f7
eduardonoguerag <eduardo.nogueraga@gmail.com>
2021-05-26 15:54:23 +0000

    Pruebas para la ordenacion de teams

--- tests/Feature/Admin/ListTeamsTest.php
+++ tests/Feature/Admin/ListTeamsTest.php
@@ -2,7 +2,9 @@
 
 namespace Tests\Feature\Admin;
 
+use App\Profession;
 use App\Team;
+use App\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -57,5 +59,87 @@ class ListTeamsTest extends TestCase
                 'Alpacas Manuel',
                 'Benancio Cortesistos'
             ]);
+    }
+
+    /** @test */
+    function teams_are_ordered_by_name()
+    {
+        $team1 = Team::factory()->create(['name' => 'Destileria Juanfran']);
+        $team2 = Team::factory()->create(['name' => 'Alpacas Manu']);
+        $team3 = Team::factory()->create(['name' => 'Zanguangos INC']);
+
+        $response = $this->get(route('teams.index', ['order' => 'nombre_empresa']));
+        $response->assertOk();
+        $response->assertSeeInOrder([
+            $team2->name,
+            $team1->name,
+            $team3->name,
+        ]);
+
+        $response = $this->get(route('teams.index', ['order' => 'nombre_empresa-desc']));
+        $response->assertOk();
+        $response->assertSeeInOrder([
+            $team3->name,
+            $team1->name,
+            $team2->name,
+        ]);
+
+    }
+
+    /** @test */
+    function teams_are_ordered_by_workers()
+    {
+        $team1 = Team::factory()->create();
+        $team2 = Team::factory()->create();
+        $team3 = Team::factory()->create();
+
+        User::factory()->times(3)->create(['team_id' => $team1->id]);
+        User::factory()->times(2)->create(['team_id' => $team2->id]);
+        User::factory()->times(5)->create(['team_id' => $team3->id]);
+
+        $response = $this->get(route('teams.index', ['order' => 'trabajadores']));
+        $response->assertOk();
+        $response->assertSeeInOrder([
+            $team2->name,
+            $team1->name,
+            $team3->name,
+        ]);
+
+        $response = $this->get(route('teams.index', ['order' => 'trabajadores-desc']));
+        $response->assertOk();
+        $response->assertSeeInOrder([
+            $team3->name,
+            $team1->name,
+            $team2->name
+        ]);
+
+    }
+
+    /** @test */
+    function teams_are_ordered_by_professions()
+    {
+        $profession1 = Profession::factory()->create();
+        $profession2 = Profession::factory()->create();
+        $profession3 = Profession::factory()->create();
+
+        $team1 = Team::factory()->create();
+        $team2 = Team::factory()->create();
+
+        $team1->professions()->attach([$profession1->id, $profession2->id, $profession3->id]);
+        $team2->professions()->attach([$profession2->id]);
+
+        $response = $this->get(route('teams.index', ['order' => 'numero_profesiones']));
+        $response->assertOk();
+        $response->assertSeeInOrder([
+            $team2->name,
+            $team1->name
+        ]);
+
+        $response = $this->get(route('teams.index', ['order' => 'numero_profesiones-desc']));
+        $response->assertOk();
+        $response->assertSeeInOrder([
+            $team1->name,
+            $team2->name
+        ]);
     }
 }
