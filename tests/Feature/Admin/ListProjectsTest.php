8754a0d83102dbb0c0beaf3408e9a271e5d8bf1e
eduardonoguerag <eduardo.nogueraga@gmail.com>
2021-06-06 12:30:55 +0000

    Pruebas para la ordernacion de proyectos

--- tests/Feature/Admin/ListProjectsTest.php
+++ tests/Feature/Admin/ListProjectsTest.php
@@ -60,4 +60,97 @@ class ListProjectsTest extends TestCase
 
     }
 
+    /** @test  */
+    public function projects_are_ordered_by_title()
+    {
+        Project::factory()->create(['title' => 'Sus Project']);
+        Project::factory()->create(['title' => 'A web project']);
+        Project::factory()->create(['title' => 'ZZZ Propjects']);
+
+        $this->get(route('projects.index', ['order' => 'titulo']))
+            ->assertStatus(200)
+            ->assertSeeInOrder([
+                'A web project',
+                'Sus Project',
+                'ZZZ Propjects'
+            ]);
+
+        $this->get(route('projects.index', ['order' => 'titulo-desc']))
+            ->assertStatus(200)
+            ->assertSeeInOrder([
+                'ZZZ Propjects',
+                'Sus Project',
+                'A web project',
+            ]);
+    }
+
+    /** @test  */
+    public function projects_are_ordered_budget()
+    {
+        Project::factory()->create(['budget' => 3000]);
+        Project::factory()->create(['budget' => 9000]);
+        Project::factory()->create(['budget' => 1500]);
+
+        $this->get(route('projects.index', ['order' => 'presupuesto']))
+            ->assertStatus(200)
+            ->assertSeeInOrder([
+                1500,
+                3000,
+                9000,
+            ]);
+
+        $this->get(route('projects.index', ['order' => 'presupuesto-desc']))
+            ->assertStatus(200)
+            ->assertSeeInOrder([
+                9000,
+                3000,
+                1500,
+            ]);
+
+    }
+
+    /** @test  */
+    public function projects_are_ordered_status()
+    {
+       $current = Project::factory()->create(['status' => false]);
+       $finished = Project::factory()->create(['status' => true]);
+
+        $this->get(route('projects.index', ['order' => 'estado']))
+            ->assertStatus(200)
+            ->assertSeeInOrder([
+                $current->title,
+                $finished->title,
+            ]);
+
+        $this->get(route('projects.index', ['order' => 'estado-desc']))
+            ->assertStatus(200)
+            ->assertSeeInOrder([
+                $finished->title,
+                $current->title,
+            ]);
+
+    }
+
+    /** @test  */
+    public function projects_are_ordered_finish_date()
+    {
+        $ongoing = Project::factory()->create(['finish_date' => now()->addDays(30)]);
+        $expired = Project::factory()->create(['finish_date' => now()->subDays(30)]);
+
+        $this->get(route('projects.index', ['order' => 'plazo']))
+            ->assertStatus(200)
+            ->assertSeeInOrder([
+                $expired->title,
+                $ongoing->title,
+            ]);
+
+        $this->get(route('projects.index', ['order' => 'plazo-desc']))
+            ->assertStatus(200)
+            ->assertSeeInOrder([
+                $ongoing->title,
+                $expired->title,
+            ]);
+
+    }
+
 }
